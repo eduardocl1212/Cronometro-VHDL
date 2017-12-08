95fbe9ba4ebc4fc4954c5e2ee70b8c0bb2a3dddb
Lucas Henrique Trigueiro <lucashtrigueiro@gmail.com>
2017-12-08 01:16:37 -0200

    Remoção de arquivos do projeto

--- Projeto/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "C:/Users/Lucas/Desktop/lucas logica/cronometro/Teste_isim_beh.exe" -prj "C:/Users/Lucas/Desktop/lucas logica/cronometro/Teste_beh.prj" "work.Teste" 
